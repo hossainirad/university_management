170c5dbf4d63b59f891012820606470bd917e0c3
ranashamim <ranashamimnasab@yahoo.com>
2022-02-07 09:12:33 +0330

    git add user model

--- root_dir/user/views.py
+++ root_dir/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.generics import UpdateAPIView, RetrieveAPIView, ListAPIView
+
+User = get_user_model
+
+class ShowClassesAPIView(ListAPIView):
+    pass
