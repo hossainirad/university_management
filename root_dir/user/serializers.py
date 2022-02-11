7adeb25e217bb2e355b063ec8f4fa3d1dcd09502
ranashamim <ranashamimnasab@yahoo.com>
2022-02-12 02:40:55 +0330

    some updates in lists

--- root_dir/user/serializers.py
+++ root_dir/user/serializers.py
@@ -1,10 +1,11 @@
+from cgitb import lookup
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import ClassModel, User
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class ShowUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name']
@@ -12,25 +13,13 @@ class StudentSerializer(serializers.ModelSerializer):
 class ShowClassesSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     teacher = serializers.CharField()
-    student = StudentSerializer(read_only=True, many=True)
+    student = ShowUserSerializer(read_only=True, many=True)
     created_at = serializers.DateTimeField()
 
     class Meta:
         model = ClassModel
-        fields = ['name', 'teacher', 'student', 'created_at']
-        
-    
-
-class ShowTeacherClassesSerializer(serializers.ModelSerializer):
-    name = serializers.CharField()
-    teacher = serializers.CharField()
-    student = StudentSerializer(read_only=True, many=True)
-    created_at = serializers.DateTimeField()
-
-    class Meta:
-        model = ClassModel
-        fields = ['name', 'teacher', 'student', 'created_at']    
-    
+        fields = ['id', 'name', 'teacher', 'student', 'created_at']
+            
 
 class UpdateClassesSerializer(serializers.ModelSerializer):
     class Meta:
--- root_dir/user/urls.py
+++ root_dir/user/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import ( ShowClassesAPIView, ShowTeacherClassesAPIView, UpdateClassNameAPIView)
+from .views import ( 
+    ShowClassesAPIView, 
+    ShowTeacherClassesAPIView, 
+    UpdateClassNameAPIView, 
+    ShowStudentClassesAPIView, 
+    DeleteLessonByStudentAPIView, 
+    ShowUsersAPIView
+    )
 
 urlpatterns = [
     path('classes/', ShowClassesAPIView.as_view()),
     path('updateClass/<pk>/', UpdateClassNameAPIView.as_view()),
-  #  path('deleteClass/<pk>/', DeleteClassAPIView.as_view()),
-    path('teacherClasses/<pk>', ShowTeacherClassesAPIView.as_view()),
+    path('deleteLesson/<pk>/', DeleteLessonByStudentAPIView.as_view()),     
+    path('teacherClasses/<pk>/', ShowTeacherClassesAPIView.as_view()),     
+    path('studentClasses/<pk>/', ShowStudentClassesAPIView.as_view()),
+    path('users/', ShowUsersAPIView.as_view()),
 ]
