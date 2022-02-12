bf231a3396d3cc35d1246a791042930b05cff3b7
ranashamim <ranashamimnasab@yahoo.com>
2022-02-12 17:08:40 +0330

    add some views

--- root_dir/user/urls.py
+++ root_dir/user/urls.py
@@ -7,14 +7,16 @@ from .views import (
     UpdateClassNameAPIView, 
     ShowStudentClassesAPIView, 
     DeleteLessonByStudentAPIView, 
-    ShowUsersAPIView
+    ShowUsersAPIView,
+    DeleteStudentAPIView
     )
 
 urlpatterns = [
     path('classes/', ShowClassesAPIView.as_view()),
     path('updateClass/<pk>/', UpdateClassNameAPIView.as_view()),
-    path('deleteLesson/<pk>/', DeleteLessonByStudentAPIView.as_view()),     
-    path('teacherClasses/<pk>/', ShowTeacherClassesAPIView.as_view()),     
-    path('studentClasses/<pk>/', ShowStudentClassesAPIView.as_view()),
+    path('deleteLesson/<pk>/', DeleteLessonByStudentAPIView.as_view()),  
+    path('deletestudent/<pk>/<id>/', DeleteStudentAPIView.as_view()), # pk = class ID , id = student ID       
+    path('teacherClasses/', ShowTeacherClassesAPIView.as_view()),     
+    path('studentClasses/', ShowStudentClassesAPIView.as_view()),
     path('users/', ShowUsersAPIView.as_view()),
 ]
